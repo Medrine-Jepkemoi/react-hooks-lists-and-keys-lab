a42784318d827e34659302587d155e0a7eb1d599
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-01 22:13:23 +0300

    Links added to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
-import React from "react";
-
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {
+        /* display an <a> tag for each link here */
+        links.map((link) => {
+          const navi = `#${link}`;
+          // console.log(navi)
+          return (
+            <a key={link} href={navi}>
+              {" "}
+              {link}
+            </a>
+          );
+        })
+      }
+    </nav>
+  );
 }
 
 export default NavBar;
